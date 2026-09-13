43da6c0b18156e65555bb520147b6f5d531fa4ce
Jamie Cropley <jamie.cropley@protonmail.com>
2026-09-13 15:38:10 +0100

    Add pkg-config metadata for Sokol

--- Formula/sokol.rb
+++ Formula/sokol.rb
@@ -7,18 +7,29 @@ class Sokol < Formula
   def install
     (include/"sokol").install Dir["*.h"]
     (include/"sokol/util").install Dir["util/*.h"]
+
+    (lib/"pkgconfig").mkpath
+    (lib/"pkgconfig/sokol.pc").write <<~EOS
+      prefix=#{prefix}
+      includedir=${prefix}/include/sokol
+
+      Name: sokol
+      Description: Minimal cross-platform standalone C headers
+      Version: #{version}
+      Cflags: -I${includedir}
+    EOS
   end
 
   test do
     (testpath/"test.c").write <<~C
-            #define SOKOL_IMPL
-            #include "sokol_time.h"
+      #define SOKOL_IMPL
+      #include "sokol_time.h"
 
-            int main(void) {
-              stm_setup();
-      (void)stm_now();
-      return 0;
-            }
+      int main(void) {
+        stm_setup();
+        (void)stm_now();
+        return 0;
+      }
     C
 
     system ENV.cc, "-I#{include}/sokol", "test.c", "-o", "test"
